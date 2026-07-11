f67106699781a7a4d2ad6a61b4ced571bbec2f52
ignatn7082 <nttanhn@gmail.com>
2026-07-11 10:43:11 +0700

    khong luu epub

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0 Safari/537.36'
 }
 
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+# os.makedirs(UPLOAD_FOLDER, exist_ok=True)
